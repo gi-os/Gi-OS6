8ac9dd78a823939b3e9092f5f3ef0f025e62ee2e
DCLLS <dhruvmarkcollins@gmail.com>
2016-03-04 22:44:27 +0530

    Update BGGen.js

--- BGGen.js
+++ BGGen.js
@@ -2,4 +2,4 @@ var pattern = Trianglify({
     width: window.innerWidth,
     height: window.innerHeight
   });
- document.body.style.backgroundImage = "url(pattern.svg();)";
+document.body.style.backgroundImage = 'url('+pattern.png();+');'
