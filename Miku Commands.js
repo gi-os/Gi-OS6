07e32c2f578a185dd94f0b4db6e01a3f6dc8cc72
gi-os <gio@gi-os.com>
2016-03-31 13:19:26 -0400

    Update Miku Commands.js

--- Miku Commands.js
+++ Miku Commands.js
@@ -1,56 +1,183 @@
-/*response formulation code*/
-function makeresponse() {
-  var input = [ document.getElementById("say"),
-               
-               ]
-
-  var text = document.getElementById("say");
-  var res = document.getElementById("response");
-  //hi
-  if (text.value === "hello" || text.value === "hi" || text.value === "Hi! What's Up?") {
-    res.innerHTML = "Hi! how are you?";
-    //response good               
-  } else if (text.value === "i am good") {
-    res.innerHTML = "Ok Great!";
-    //response good               
-  } else if (text.value === "i am not good") {
-    res.innerHTML = "That's too bad.";
-    //bye
-  } else if (text.value === "bye") {
-    res.innerHTML = "Goodbye";
-    //question existence
-  } else if (text.value === "what are you" || text.value === "who are you") {
-    res.innerHTML = "Im miku! your personal assistant.";
-    //what is life                   
-  } else if (text.value === "what is life") {
-    res.innerHTML = "Im going to need time to think over that.";
-    //open the pod bay doors            
-  } else if (text.value === "open the pod bay doors") {
-    res.innerHTML = "Im sorry dave, im afraid I can't do that.";
-    //fav color                   
-  } else if (text.value === "what is your favorite color") {
-    res.innerHTML = "my favorite color is blue!";
-    //love                  
-  } else if (text.value === "i love you") {
-    res.innerHTML = "I think we should just be friends...";
-    //feel                
-  } else if (text.value === "how are you") {
-    res.innerHTML = "I am a robot, I do not feel.";
-    //feel                
-  } else if (text.value === "what is love") {
-    res.innerHTML = "Baby dont hurt me, dont hurt me, no more.";
-    //life              
-  } else if (text.value === "do i have a life") {
-    res.innerHTML = "No, because you are wasting your time talking to me.";
-    //talk             
-  } else if (text.value === "let's talk") {
-    res.innerHTML = "Ok!";
-    //no response made
-  } else {
-    res.innerHTML = "Hello There!";
-  }
-  //COPY AND PASTE CODE    }else if (text.value === "INSERT WORD") { res.innerHTML = "INSERT RESPONSE";
-
+var bootup = document.getElementsByClassName('bootbox')[0];
+function loadup() {
+   bootup.classList.add('zoom');
 }
+function startDictation() {
 
-var t = setInterval(makeresponse, 1000)
+   if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+     var recognition = new webkitSpeechRecognition();
+
+     recognition.continuous = false;
+     recognition.interimResults = false;
+
+     recognition.lang = "en-US";
+     recognition.start();
+
+     recognition.onresult = function(e) {
+       var t = setTimeout(function(){responsiveVoice.speak(document.getElementById("response").innerHTML);}, 1000)
+
+       document.getElementById('transcript').value = e.results[0][0].transcript;
+       recognition.stop();
+       document.getElementById('labnol').submit();
+       
+       
+     };
+
+     recognition.onerror = function(e) {
+       recognition.stop();
+     }
+
+   }
+ }
+
+ 
+
+//weather
+
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'Washington DC',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      html = 'It is '+weather.temp+'&deg;'+weather.units.temp+'';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html(''+error+'');
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*response formulation code*/
+ function makeresponse() {
+   var input = [document.getElementById("transcript"),
+
+   ]
+
+   var text = document.getElementById("transcript");
+   var res = document.getElementById("response");
+   //hi
+   if (text.value === "Hello computer" || text.value === "hello computer" || text.value === "Hi! What's Up?") {
+     res.innerHTML = "Hi! how are you?";
+     
+     //response good               
+   } else if (text.value === "I am good computer") {
+     res.innerHTML = "Ok Great!";
+     //response good               
+   } else if (text.value === "I am not good computer") {
+     res.innerHTML = "That's too bad.";
+     //bye
+   } else if (text.value === "goodbye computer" || text.value === "goodnight computer") {
+     res.innerHTML = "Goodbye Gio!";
+     //question existence
+   } else if (text.value === "what are you computer" || text.value === "who are you") {
+     res.innerHTML = "I'm your personal assistant gio! How could you forget!";
+     //what is life                   
+   } else if (text.value === "what is life computer") {
+     res.innerHTML = "I'm going to need time to think that over.";
+     //open the pod bay doors            
+   } else if (text.value === "open the pod bay doors computer") {
+     res.innerHTML = "I'm sorry dave, im afraid I can't do that.";
+     //fav color                   
+   } else if (text.value === "what is your favorite color computer") {
+     res.innerHTML = "My favorite color is blue!";
+     //love                  
+   } else if (text.value === "I love you computer") {
+     res.innerHTML = "I think we should just be friends...";
+     //feel                
+   } else if (text.value === "how are you computer") {
+     res.innerHTML = "I am a robot, I do not feel.";
+     //feel                
+   } else if (text.value === "what is love computer") {
+     res.innerHTML = "Baby dont hurt me, dont hurt me, no more.";
+     //life              
+   } else if (text.value === "do i have a life computer") {
+     res.innerHTML = "No, because you are wasting your time talking to me.";
+     //talk             
+   } else if (text.value === "let's talk computer") {
+     res.innerHTML = "Ok!";
+     //no response made
+
+     //New MIKU PE only comands
+   } else if (text.value === "good morning computer") {
+     res.innerHTML = "Good Morning Gio!";
+   } else if (text.value === "good night computer") {
+     res.innerHTML = "Good Night Gio!";
+     } else if (text.value === "is my computer on computer") {
+     res.innerHTML = "I believe so.";
+} else if (text.value === "do you love me") {
+     res.innerHTML = "Of course I love you!";
+  } else if (text.value === "let's dance") {
+     res.innerHTML = "Ok, but I am not the best dancer.";
+    
+    
+    //Work comands
+      } else if (text.value === "what time is it computer") {
+startTime();
+var t = setTimeout(function(){responsiveVoice.speak(document.getElementById("txt").innerHTML); }, 1000) 
+emptytime();
+      } else if (text.value === "what is the weather computer") {
+
+var t = setTimeout(function(){responsiveVoice.speak(document.getElementById("weather").innerHTML); }, 1000) 
+emptyweather()
+   } else {
+     res.innerHTML = "";
+   }
+   //COPY AND PASTE CODE    }else if (text.value === "INSERT WORD") { res.innerHTML = "INSERT RESPONSE";
+
+ }
+
+ var t = setInterval(makeresponse, 1000)
+     
+       
+
+
+ 
+function emptytime(){ document.getElementById('txt').value = "";
+   document.getElementById('transcript').value = "";
+                   
+}
+function emptyweather(){ document.getElementById('txt').value = "";
+   document.getElementById('transcript').value = "";
+                   
+}
+function Talkback() {  responsiveVoice.speak(document.getElementById("response").innerHTML);
+ }
+function Talktime() {
+responsiveVoice.speak(document.getElementById("txt").innerHTML);
+}
